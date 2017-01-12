063b4f710c718b035ccc9c6c887cbda66ec495de
Sum Proxy <sum.proxy@gmail.com>
2017-01-12 17:25:03 +0300

    Move App init code into app.rs

--- src/app.rs
+++ src/app.rs
@@ -1,11 +1,14 @@
 extern crate gfx;
 extern crate glutin;
+extern crate gfx_window_glutin;
+
+use gfx::traits::FactoryExt;
 
 use self::glutin::Event::{Closed, KeyboardInput};
 use self::glutin::VirtualKeyCode::{Left, Right, Up, Down, Space, Escape};
 use self::glutin::ElementState::Pressed;
 
-use super::pipe;
+use common::{ColorFormat, DepthFormat, Screen, pipe};
 
 pub struct App<R, C, D>
     where R: gfx::Resources,
@@ -17,6 +20,8 @@ pub struct App<R, C, D>
     pub device: D,
     pub slice: gfx::Slice<R>,
     pub pso: gfx::PipelineState<R, pipe::Meta>,
+    pub data: pipe::Data<R>,
+    pub screen: Screen,
 }
 
 impl<R, C, D> App<R, C, D>
@@ -24,16 +29,47 @@ impl<R, C, D> App<R, C, D>
           C: gfx::CommandBuffer<R>,
           D: gfx::Device,
 {
-    pub fn new(w: glutin::Window,
-               e: gfx::Encoder<R, C>,
-               d: D,
-               s: gfx::Slice<R>,
-               pso: gfx::PipelineState<R, pipe::Meta>) -> Self {
-        App { window: w,
-              encoder: e,
-              device: d,
-              slice: s,
+    pub fn new(
+        // w: glutin::Window,
+        //        e: gfx::Encoder<R, C>,
+        //        d: D,
+        //        s: gfx::Slice<R>,
+        //        pso: gfx::PipelineState<R, pipe::Meta>,
+        //        data: pipe::Data<R>
+    ) -> Self {
+
+        let screen = Screen::new();
+        
+        let builder = glutin::WindowBuilder::new()
+            .with_title("Tetris!".to_string())
+            .with_dimensions(screen.width(), screen.height());
+
+        let (window, mut device, mut factory, main_color, _main_depth) =
+            gfx_window_glutin::init::<ColorFormat, DepthFormat>(builder);
+
+        let encoder: gfx::Encoder<_, _> = factory.create_command_buffer().into();
+
+        let pso = factory.create_pipeline_simple(
+            include_bytes!("shader/triangle_150.glslv"),
+            include_bytes!("shader/triangle_150.glslf"),
+            pipe::new()).unwrap();
+
+        let (vertex_buffer, slice) = factory.create_vertex_buffer_with_slice(&screen.elem.vertices, &screen.elem.indices as &[u16]);
+
+        let data = pipe::Data {
+            color: [1.0, 0.0, 0.0],
+            center: [0.0, 0.0],
+            vbuf: vertex_buffer,
+            out: main_color
+        };
+
+        App::<R, C, D> { window: window,
+              encoder: encoder,
+              device: device,
+              slice: slice,
               pso: pso,
+              data: data,
+              screen: screen,
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 #[macro_use]
 extern crate gfx;
 extern crate glutin;
-extern crate gfx_window_glutin;
 
-use gfx::traits::FactoryExt;
 use gfx::Device;
 
 use glutin::Event::{Closed, KeyboardInput};
@@ -15,36 +13,8 @@ mod app;
 
 use app::App;
 
-use common::{ColorFormat, DepthFormat, Screen, pipe};
-
-
 fn main() {
-    let screen = Screen::new();
-    
-    let builder = glutin::WindowBuilder::new()
-        .with_title("Tetris!".to_string())
-        .with_dimensions(screen.width(), screen.height());
-
-    let (window, mut device, mut factory, main_color, _main_depth) =
-        gfx_window_glutin::init::<ColorFormat, DepthFormat>(builder);
-
-    let encoder: gfx::Encoder<_, _> = factory.create_command_buffer().into();
-
-    let pso = factory.create_pipeline_simple(
-        include_bytes!("shader/triangle_150.glslv"),
-        include_bytes!("shader/triangle_150.glslf"),
-        pipe::new()).unwrap();
-
-    let (vertex_buffer, slice) = factory.create_vertex_buffer_with_slice(&screen.elem.vertices, &screen.elem.indices as &[u16]);
-
-    let data = pipe::Data {
-        color: [1.0, 0.0, 0.0],
-        center: [0.0, 0.0],
-        vbuf: vertex_buffer,
-        out: main_color
-    };
-
-    let mut app = App::new(window, encoder, device, slice, pso);
+    let mut app = App::new();//window, encoder, device, slice, pso, data);
 
     'main: loop {
         // loop over events
@@ -72,8 +42,8 @@ fn main() {
             }
         }
         // draw a frame
-        app.encoder.clear(&data.out, screen.clear_color);
-        app.encoder.draw(&app.slice, &app.pso, &data);
+        app.encoder.clear(&app.data.out, app.screen.clear_color);
+        app.encoder.draw(&app.slice, &app.pso, &app.data);
         app.encoder.flush(&mut app.device);
         app.window.swap_buffers().unwrap();
         app.device.cleanup();
