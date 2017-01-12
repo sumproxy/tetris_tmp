29f0171a8f1b2c611645785567fb38d323272388
Sum Proxy <sum.proxy@gmail.com>
2017-01-12 18:29:58 +0300

    Move new into a proper fn

--- src/app.rs
+++ src/app.rs
@@ -29,48 +29,52 @@ impl<R, C, D> App<R, C, D>
           C: gfx::CommandBuffer<R>,
           D: gfx::Device,
 {
-    pub fn new(
-        // w: glutin::Window,
-        //        e: gfx::Encoder<R, C>,
-        //        d: D,
-        //        s: gfx::Slice<R>,
-        //        pso: gfx::PipelineState<R, pipe::Meta>,
-        //        data: pipe::Data<R>
-    ) -> Self {
-
-        let screen = Screen::new();
-        
-        let builder = glutin::WindowBuilder::new()
-            .with_title("Tetris!".to_string())
-            .with_dimensions(screen.width(), screen.height());
-
-        let (window, mut device, mut factory, main_color, _main_depth) =
-            gfx_window_glutin::init::<ColorFormat, DepthFormat>(builder);
-
-        let encoder: gfx::Encoder<_, _> = factory.create_command_buffer().into();
-
-        let pso = factory.create_pipeline_simple(
-            include_bytes!("shader/triangle_150.glslv"),
-            include_bytes!("shader/triangle_150.glslf"),
-            pipe::new()).unwrap();
-
-        let (vertex_buffer, slice) = factory.create_vertex_buffer_with_slice(&screen.elem.vertices, &screen.elem.indices as &[u16]);
-
-        let data = pipe::Data {
-            color: [1.0, 0.0, 0.0],
-            center: [0.0, 0.0],
-            vbuf: vertex_buffer,
-            out: main_color
-        };
-
-        App::<R, C, D> { window: window,
-              encoder: encoder,
-              device: device,
-              slice: slice,
-              pso: pso,
-              data: data,
-              screen: screen,
-        }
-    }
 }
    
+pub fn new<R, C, D>(
+    w: glutin::Window,
+    e: gfx::Encoder<R, C>,
+    d: D,
+    s: gfx::Slice<R>,
+    pso: gfx::PipelineState<R, pipe::Meta>,
+    data: pipe::Data<R>
+) -> App<R, C, D>
+    where R: gfx::Resources,
+          C: gfx::CommandBuffer<R>,
+          D: gfx::Device,
+{
+
+    let screen = Screen::new();
+    
+    let builder = glutin::WindowBuilder::new()
+        .with_title("Tetris!".to_string())
+        .with_dimensions(screen.width(), screen.height());
+
+    let (window, mut device, mut factory, main_color, _main_depth) =
+        gfx_window_glutin::init::<ColorFormat, DepthFormat>(builder);
+
+    let encoder: gfx::Encoder<_, _> = factory.create_command_buffer().into();
+
+    let pso = factory.create_pipeline_simple(
+        include_bytes!("shader/triangle_150.glslv"),
+        include_bytes!("shader/triangle_150.glslf"),
+        pipe::new()).unwrap();
+
+    let (vertex_buffer, slice) = factory.create_vertex_buffer_with_slice(&screen.elem.vertices, &screen.elem.indices as &[u16]);
+
+    let data = pipe::Data {
+        color: [1.0, 0.0, 0.0],
+        center: [0.0, 0.0],
+        vbuf: vertex_buffer,
+        out: main_color
+    };
+
+    App::<R, C, D> { window: window,
+                         encoder: encoder,
+                         device: device,
+                         slice: slice,
+                         pso: pso,
+                         data: data,
+                         screen: screen,
+    }
+}
